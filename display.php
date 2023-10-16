1be2b4b110f9e3f9b453cc8f074cb8d5692bc510
Blaze2oi <Nazarethn321@gmail.com>
2023-10-17 00:48:17 +0530

    disp

--- display.php
+++ display.php
@@ -4,7 +4,16 @@ $query = "SELECT * FROM USERS";
 $data = mysqli_query($conn, $query);
 
 $total = mysqli_num_rows($data);
-echo $total;
-//slayyy 
 
+if($total != 0)
+{
+    while($result = mysqli_fetch_assoc($data))
+    {
+        echo $result["Name"]." ".$result["Number"]." ".$result["Gender"]." ".$result["Address"]." ".$result["Email"]."<br>";
+    }
+}
+else
+{
+    echo "No records found";
+}
 ?>
