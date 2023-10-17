8388ba1dc803269fb433b91f14e8c0cefad5d43c
Blaze2oi <Nazarethn321@gmail.com>
2023-10-17 09:52:42 +0530

    2

--- display.php
+++ display.php
@@ -7,9 +7,37 @@ $total = mysqli_num_rows($data);
 
 if($total != 0)
 {
+    ?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>User Registration Details</title>
+    <link rel="stylesheet" href="user_display.css?v=<?php echo time(); ?>">
+</head>
+<body>
+    <h1>User Registration Details</h1>
+	
+    <table border="1">
+        <tr>
+            <th>Name</th>
+            <th>Phone Number</th>
+	    <th>Gender</th>
+	    <th>Location</th>
+	    <th>Email</th>
+        </tr>
+
+    <?php
     while($result = mysqli_fetch_assoc($data))
     {
-        echo $result["Name"]." ".$result["Number"]." ".$result["Gender"]." ".$result["Address"]." ".$result["Email"]."<br>";
+        echo "<tr>
+              <td>$result[Name].</td>
+              <td>$result[Number].</td>
+	          <td>$result[Gender].</td>
+	          <td>$result[Address].</td>
+	          <td>$result[Email].</td>
+              </tr>";
+        // echo $result["Name"]." ".$result["Number"]." ".$result["Gender"]." ".$result["Address"]." ".$result["Email"]."<br>";
     }
 }
 else
