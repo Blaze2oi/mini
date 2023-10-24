5cd47d7d0153b6016efd34c94f5e01852e953027
Blaze2oi <Nazarethn321@gmail.com>
2023-10-25 01:52:25 +0530

    t3

--- appointments.php
+++ appointments.php
@@ -1,7 +1,7 @@
 <?php
 // session_start();
 include("connect.php");
-// error_reporting(0);//
+// error_reporting(0);///
 // $userprofile = $_SESSION['user_name'];
 // if($userprofile == true)
 $Email = $_GET['Email'];
